2c02984e60dd60e3c7811109b7dc7f04086c81d0
NatNgs <nathael.nogues@hotmail.fr>
2018-10-30 15:11:19 +0100

    Random players and diagonal move

--- spacejmp/main.js
+++ spacejmp/main.js
@@ -1,204 +1,289 @@
-const showGrid = 5; // ! Value x2+1 (-gridSize to gridSize)
-const mapSize = 15; // ! Value x2+1 (-mapSize to mapSize)
-const wallsProba = 0.25;
-const map = new Grid();
+const DISPLAY_SIZE = 5; // grid view size = Value×2+1 (-gridSize to gridSize)
+const MAP_SIZE = 15; // map size = Value×2+1 (-MAP_SIZE to MAP_SIZE)
+const WALL_CHANCE = 0.25; // between 0 and 1
+const WALL = 'wall'; // css class
 
 // Rotation priority list according to the last move orientation
-const rots = [{x:0,y:1},{x:-1,y:0},{x:0,y:-1},{x:1,y:0}] // 0=up,1=left,2=down,3=right
+const ROTS = [{x:0,y:1},{x:-1,y:0},{x:0,y:-1},{x:1,y:0}] // 0=up,1=left,2=down,3=right
 
 function idOf(x,y) {
 	return 'c'+x+'_'+y;
 }
 
-function Turn(nbPlayers) {
+function Grid() {
+	const THIS = this;
 	const allAstros = [];
-	
-	// // // Functions
-	function launch() {
+	const walls = new Set();
+
+	// // // Public functions
+	this.launch = function() {
 		turnA([...allAstros])
 	}
-	function turnA(whoNext) {
-		if(whoNext.length <=0) {
-			setTimeout(launch, 100)
-			return
-		}
-		
-		const curr = whoNext.shift()
-		
-		if(curr.fly()) { // try to fly (true if done, false if not flying)
-			turnA(whoNext)
-		} else {
-			refreshView(curr)
-			document.onkeydown = (e=window.event)=>{
-				//console.log('CLICK: '+e.keyCode)
-				switch(e.keyCode) {
-				case 37: // left arrow
-					if(!curr.move(1)) return
-					break
-				case 38: // up arrow
-					if(!curr.move(0)) return
-					break
-				case 39: // right arrow
-					if(!curr.move(3)) return
-					break
-				case 40: // down arrow
-					if(!curr.move(2)) return
-					break
-				default: return
-				}
-				
-				document.onkeydown = undefined
-				turnA(whoNext)
-			}
-		}
-	}
-	
-	function refreshView(astro) {
-		for(let i=-showGrid; i<=showGrid; i++) {
-			for(let j=-showGrid; j<=showGrid; j++) {
-				const cell = document.getElementById(idOf(i,j))
-				if(map.isWall(astro.x+i,astro.y+j))
-					cell.classList.add('wall')
-				else
-					cell.classList.remove('wall')
-				
-				cell.classList.remove('astro0')
-				cell.classList.remove('astro1')
-				cell.classList.remove('astro2')
-				cell.classList.remove('astro3')
-			}
-		}
 
-		for(let a of allAstros) { 
-			const astroCell = document.getElementById(idOf(a.x-astro.x, a.y-astro.y))
-			if(astroCell)
-				astroCell.classList.add('astro'+a.rot)
-		}
-	}
-	
-	this.hasSmthgOn = function(x,y) {
-		if(map.isWall(x,y))
-			return 'wall';
+	this.getCell = function(x,y) {
+		if(walls.has(idOf(x,y)))
+			return WALL;
 		for(astro of allAstros) {
 			if(astro.x === x && astro.y === y)
-				return astro;
+				return 'astro'+astro.rot;
 		}
 		return false;
 	}
-	
+
+	this.addAstro = function(astro) {
+		allAstros.push(astro)
+	}
+
+	// // // Private Functions
+	function turnA(whoNext) {
+		if(whoNext.length <=0) {
+			setTimeout(THIS.launch, 200)
+			return
+		}
+
+		const curr = whoNext.shift()
+
+		if(curr.fly()) { // try to fly (true if done, false if not flying)
+			curr.refreshView()
+			turnA(whoNext)
+		} else {
+			curr.refreshView()
+			let secur = false
+			curr.askForAction((action)=>{
+				if(secur)
+					return true // turn is already finished
+
+				if(action < -1 || action > 5
+				 || (action < 4 && !curr.move(action))
+				 || (action >= 4 && !curr.diag(action-4)))
+					return false // cannot do this move
+
+				curr.refreshView()
+				turnA(whoNext)
+				return (secur = true)
+			})
+		}
+	}
+
 	// // // Init
-	while(nbPlayers--)
-		allAstros.push(new Astro(this))
-	
-	launch();
+	// Random walls
+	for(let i=-MAP_SIZE; i<MAP_SIZE; i++)
+		for(let j=-MAP_SIZE; j<MAP_SIZE; j++)
+			if(Math.random() < WALL_CHANCE)
+				walls.add(idOf(i,j))
+
+	// world border
+	for(let i=-MAP_SIZE+1; i<MAP_SIZE; i++) {
+		walls.add(idOf(i, -MAP_SIZE))
+		walls.add(idOf(i, MAP_SIZE))
+		walls.add(idOf(-MAP_SIZE, i))
+		walls.add(idOf(MAP_SIZE, i))
+	}
 }
 
 let id=0
-function Astro(turn) {
+function Astro(grid) {
 	this.name = 'Astro'+(++id)
 	this.x;
 	this.y;
 	this.rot = 0; // rot (0=head up,1=head left,2=head down,3=head right)
-	this.lastMove;
 
 	// // // Functions
-	this.isFlying = function() {
-		return !turn.hasSmthgOn(this.x-rots[this.rot].x, this.y-rots[this.rot].y)
-	}
-	
 	/**
 	 * Return true if is flying and was moved one tile more on flight
 	 * false otherwise
 	 */
 	this.fly = function() {
-		// check if flying (nothing below him)
-		if(!this.isFlying())
-			return false
-		
-		// check if stop flying (nothing around him)
-		const landingPrio = [this.rot, (this.rot+4-1)%4, (this.rot+1)%4]
-		for(let r of landingPrio) {
-			if(turn.hasSmthgOn(this.x+rots[r].x, this.y+rots[r].y)) {
-				// make it land
-				this.rot = (r+2)%4
-				return false // this is not a move, so return false
-			}
+		if(this.isRotating
+		  && !grid.getCell(this.isRotating.x, this.isRotating.y)) {
+			this.x = this.isRotating.x
+			this.y = this.isRotating.y
+			this.rot = this.isRotating.r
+			this.isRotating = undefined
+			return true
 		}
-		
+
+		// check if flying
+		if(this.tryLand())
+			return false
+
 		// continue to fly
-		this.x += rots[this.rot].x
-		this.y += rots[this.rot].y
+		this.x += ROTS[this.rot].x
+		this.y += ROTS[this.rot].y
+
+		this.tryLand()
 		return true
 	}
-	
+
+	/**
+	 * Return true if has landed
+	 * false otherwise
+	 */
+	this.tryLand = function() {
+		const landingPrio = [2, 0, 3, 1].map(a=>(a+this.rot)%4)
+
+		for(let r of landingPrio) {
+			if(grid.getCell(this.x+ROTS[r].x, this.y+ROTS[r].y)) {
+				// make it land
+				this.rot = (r+2)%4
+				this.rotating = undefined
+				return true
+			}
+		}
+		return false
+	}
+
 	/**
 	 * Return true if can move and was moved
 	 * false otherwise
 	 */
-	this.move = function(mv = this.lastMove) {
+	this.move = function(mv) {
 		// cannot move
-		if(turn.hasSmthgOn(this.x+rots[mv].x, this.y+rots[mv].y)) {
-			this.rot = mv // currently not seen because no refresh view while selecting a move
+		if(mv < 0 || mv > 3)
 			return false
+
+		// rotate only
+		if(grid.getCell(this.x+ROTS[mv].x, this.y+ROTS[mv].y)) {
+			this.rot = (mv+2)%4
+			return true
 		}
-		
+
 		// apply move
-		this.x += rots[mv].x
-		this.y += rots[mv].y
-		this.lastMove = mv
-		
+		this.x += ROTS[mv].x
+		this.y += ROTS[mv].y
+
 		// if start flying, rotate to the direction of flight
-		if(this.isFlying())
+		if(!this.tryLand()) {
 			this.rot = mv
+		}
+
+		return true
+	}
+
+	/**
+	 * Return true if can move and was moved diagonally
+	 * false otherwise
+	 */
+	this.diag = function(lr) {
+		// lr=0: turn counterclockwise; lr=1: turn clockwise
+		const mv = lr*2+1 // mv=1: counterclockwise, 3: clockwise
+
+		const newRot = (this.rot+mv)%4
+		const side = {
+			x: this.x+ROTS[newRot].x,
+			y: this.y+ROTS[newRot].y
+		}
+		if(grid.getCell(side.x, side.y))
+			return false
+
+		const diag = {
+			x: side.x+ROTS[(this.rot+2)%4].x,
+			y: side.y+ROTS[(this.rot+2)%4].y,
+			r: newRot
+		}
+		if(grid.getCell(diag.x, diag.y))
+			return false
+
+		this.x = side.x
+		this.y = side.y
+		this.rot = newRot
+		this.isRotating = diag
 
 		return true
 	}
 
 	// // // Init
 	do {
-		this.x = ((Math.random()*mapSize*2+1)-mapSize)|0
-		this.y = ((Math.random()*mapSize*2+1)-mapSize)|0
-	} while(turn.hasSmthgOn(this.x,this.y));
-	
-	this.lastMove = (Math.random()*4)|0
-	while(this.move()); // move till next wall
-}
+		this.x = ((Math.random()*MAP_SIZE*2+1)-MAP_SIZE)|0
+		this.y = ((Math.random()*MAP_SIZE*2+1)-MAP_SIZE)|0
+	} while(grid.getCell(this.x,this.y));
 
-function Grid() {
-	let walls = new Set();
-
-	// Random walls
-	for(let i=-mapSize; i<mapSize; i++)
-		for(let j=-mapSize; j<mapSize; j++)
-			if(Math.random() < wallsProba)
-				walls.add(idOf(i,j))
-
-	// world border
-	for(let i=-mapSize+1; i<mapSize; i++) {
-		walls.add(idOf(i, -mapSize))
-		walls.add(idOf(i, mapSize))
-		walls.add(idOf(-mapSize, i))
-		walls.add(idOf(mapSize, i))
-	}
-
-	this.isWall = function(x,y) {
-		return walls.has(idOf(x,y))
-	}
+	const themove = (Math.random()*4)|0
+	while(this.fly(themove)); // move till next wall
 }
 
 function init() {
 	// generate grid
-	const grid = document.getElementById('grid');
+	const display = document.getElementById('grid');
 
-	let gridText = '';
-	for(let y=-showGrid; y<=showGrid; y++) {
-		gridText += '<tr>'
-		for(let x=-showGrid; x<=showGrid; x++)
-			gridText += '<td id="'+idOf(x,-y)+'" class="cell"></td>'
-		gridText += '</tr>'
+	let html = '';
+	for(let y=DISPLAY_SIZE; y>=-DISPLAY_SIZE; --y) {
+		html += '<tr>'
+		for(let x=-DISPLAY_SIZE; x<=DISPLAY_SIZE; x++)
+			html += '<td id="'+idOf(x,y)+'" class="cell"></td>'
+		html += '</tr>'
 	}
-	grid.innerHTML = gridText;
+	display.innerHTML = html;
+
 
 	// launch
-	new Turn(2);
+	const grid = new Grid();
+
+	// Generate players
+	grid.addAstro(generateManPlayer(grid))
+	for(let i=5; i>=0; --i)
+		grid.addAstro(generateBotPlayer(grid))
+
+	grid.launch()
+}
+
+function generateManPlayer(grid) {
+	const astro = new Astro(grid);
+	astro.refreshView = ()=>{
+		for(let i=-DISPLAY_SIZE; i<=DISPLAY_SIZE; i++) {
+			for(let j=-DISPLAY_SIZE; j<=DISPLAY_SIZE; j++) {
+				const cell = document.getElementById(idOf(i,j)).classList
+				cell.remove(WALL)
+				cell.remove('astro0')
+				cell.remove('astro1')
+				cell.remove('astro2')
+				cell.remove('astro3')
+
+				const somethingOnCell = grid.getCell(astro.x+i,astro.y+j)
+				if(somethingOnCell)
+					cell.add(somethingOnCell)
+			}
+		}
+	}
+
+	let shift = false;
+	let call;
+	document.onkeydown = (e=window.event)=>{
+		if(e.keyCode === 16) {
+			shift = true
+		}
+		else if(call)
+			call(e.keyCode, shift)
+	}
+	document.onkeyup = (e=window.event)=>{
+		if(e.keyCode === 16)
+			shift = false
+	}
+	astro.askForAction = (cb)=>{
+		call = (key, shift)=>{
+			//console.log((shift?'shift+':'')+key)
+			if(
+			  (shift &&
+			    (  (key === 37 && cb(4)) // shift left
+			    || (key === 39 && cb(5)) // shift right
+			    || (key === 40 && cb(-1)))) // shift down
+			  || (!shift &&
+			    (  (key === 37 && cb(1)) // left
+			    || (key === 38 && cb(0)) // up
+			    || (key === 39 && cb(3)) // right
+			    || (key === 40 && cb(2)))) // down
+			)
+				call = undefined
+		}
+	};
+	return astro
+}
+
+function generateBotPlayer(grid) {
+	const astro = new Astro(grid);
+	astro.refreshView = ()=>{}
+	astro.askForAction = cb=>{
+		while(!cb((Math.random()*6-1)|0));
+	}
+	return astro
 }
